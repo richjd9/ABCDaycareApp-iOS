4dd7642c8b34dfb6a90894c9fb93e0581d1d8665
John Russell <shvffle@gmail.com>
2016-03-24 14:20:04 -0400

    Initial Commit

---
+++ ABCD/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ABCD
+//
+//  Created by John Russell on 3/24/16.
+//  Copyright © 2016 John Russell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
