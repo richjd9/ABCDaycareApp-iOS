6a979c85d0091cbca0a68c81f9e588b800f74ba0
Ching C <Pizza@chings-macbook-pro.local>
2016-03-31 14:33:45 -0400

    Initial Commit

---
+++ ABCDTests/ABCDTests.swift
@@ -0,0 +1,36 @@
+//
+//  ABCDTests.swift
+//  ABCDTests
+//
+//  Created by John Russell on 3/24/16.
+//  Copyright © 2016 John Russell. All rights reserved.
+//
+
+import XCTest
+@testable import ABCD
+
+class ABCDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
