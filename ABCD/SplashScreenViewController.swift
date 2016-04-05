479df032d548b49ff63cb0cbe5ea9c6d396e6887
armendo <Fer@Fers-MacBook-Pro.local>
2016-04-05 15:51:14 -0400

    SplashScreen with Button and Logo
    The splash video screen video has the current logo and a button to
    segue to the mainscreen.

--- ABCD/SplashScreenViewController.swift
+++ ABCD/SplashScreenViewController.swift
@@ -11,33 +11,38 @@ import VideoSplashKit
 
 
 class SplashScreenViewController: VideoSplashViewController {
-  
-  
-  
-  
   private var firstAppear = true
   
+  @IBAction func signIn(sender: AnyObject) {
+    continueToMainScreen()
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
+  
+    //Getting the Splash Video Url.
+    let mainBundle = NSBundle.mainBundle()
+    let file = mainBundle.pathForResource("SplashVideo", ofType: "mp4")
+    let url = NSURL.fileURLWithPath(file!)
     
-//    let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("SplashVideo", ofType: "mp4")!)
-//    self.videoFrame = view.frame
-//    self.fillMode = .ResizeAspectFill
-//    self.alwaysRepeat = true
-//    self.sound = true
-//    
-//    //The Video
-//    self.startTime = 72.0
-//    self.duration = 10.0
-//    self.alpha = 0.7
-//    self.backgroundColor = UIColor.blackColor()
-//    self.contentURL = url
-//    self.restartForeground = true
+    //Setting the Video Options
+    self.videoFrame = view.frame
+    self.fillMode = .ResizeAspectFill
+    self.startTime = 72.0
+    self.duration = 10.0
+    self.alpha = 0.7
+    self.backgroundColor = UIColor.blackColor()
     
-    NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: #selector(SplashScreenViewController.timeToMoveOn), userInfo: nil, repeats: false)
+    //Playing the video
+    self.contentURL = url
+    self.restartForeground = true
+    
+    //Timer to segue into mainscreen after 10 seconds.
+    NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: #selector(SplashScreenViewController.continueToMainScreen), userInfo: nil, repeats: false)
     
   }
-  func timeToMoveOn() {
+  
+  //Helper Function to segue to mainScreen.
+  func continueToMainScreen() {
     self.performSegueWithIdentifier("mainViewController", sender: self)
   }
 }
