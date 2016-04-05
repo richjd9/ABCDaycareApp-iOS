f1259a80859d2bc0abc4d5d343c7f7af91d163cc
armendo <Fer@Fers-MacBook-Pro.local>
2016-04-05 14:50:56 -0400

    SplashVideo Being Added
    Cocoa pods added. SwiftyJson and VideoSplashKit pods added.

---
+++ ABCD/SplashScreenViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SplashScreenViewController.swift
+//  ABCD
+//
+//  Created by Fer on 4/3/16.
+//  Copyright © 2016 John Russell. All rights reserved.
+//
+
+import UIKit
+import VideoSplashKit
+
+
+class SplashScreenViewController: VideoSplashViewController {
+  
+  
+  
+  
+  private var firstAppear = true
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+//    let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("SplashVideo", ofType: "mp4")!)
+//    self.videoFrame = view.frame
+//    self.fillMode = .ResizeAspectFill
+//    self.alwaysRepeat = true
+//    self.sound = true
+//    
+//    //The Video
+//    self.startTime = 72.0
+//    self.duration = 10.0
+//    self.alpha = 0.7
+//    self.backgroundColor = UIColor.blackColor()
+//    self.contentURL = url
+//    self.restartForeground = true
+    
+    NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: #selector(SplashScreenViewController.timeToMoveOn), userInfo: nil, repeats: false)
+    
+  }
+  func timeToMoveOn() {
+    self.performSegueWithIdentifier("mainViewController", sender: self)
+  }
+}
---
+++ Pods/Target Support Files/Pods-ABCD/Pods-ABCD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ABCD : NSObject
+@end
+@implementation PodsDummy_Pods_ABCD
+@end
